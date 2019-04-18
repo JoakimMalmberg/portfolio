00169783ac3fb1f0bb9a272b1960b837c8bfb782
Joakim <jmalmberg.web@gmail.com>
2019-04-18 19:21:48 +0200

    Påbörjat Hero och fixat med bootstrap responsiveness

--- front-page.php
+++ front-page.php
@@ -18,29 +18,21 @@ get_header();
 
 $container = get_theme_mod( 'understrap_container_type' ); ?>
 
-<div class="container">
-	<div class="row row-offcanvas row-offcanvas-right">
-
-		<div class="col-12 full-page hero">
-			<div>
-				<h2>Joakim Malmberg</h2>
-				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-					mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-					magna mollis euismod. Donec sed odio dui. </p>
-
+<div class="container-fluid">
+	<div>
+		
+		<div class="row full-page hero">
+			<div class="col-12">
+				<?php get_template_part( 'global-templates/hero' ); ?>
 			</div>
 		</div>
 
 		<div class="row full-page usps">
-			<div class="col-6">
+			<div class="col-12 col-lg-6">
 				<h2>Usp 1</h2>
-				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-					mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-					magna mollis euismod. Donec sed odio dui. </p>
-				<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
 			</div>
 			<!--/span-->
-			<div class="col-6">
+			<div class="col-12 col-lg-6">
 				<h2>Usp 2</h2>
 				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
 					mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
@@ -52,57 +44,44 @@ $container = get_theme_mod( 'understrap_container_type' ); ?>
 		<!--/row-->
 
 		<div class="row full-page sites">
-			<div class="col-6 col-lg-4">
+			<div class="col-12 col-md-6 col-lg-4">
 				<h2>Site 1</h2>
 				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
 					mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
 					magna mollis euismod. Donec sed odio dui. </p>
-				<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
 			</div>
 			<!--/span-->
-			<div class="col-6 col-lg-4">
+			<div class="col-12 col-md-6 col-lg-4">
 				<h2>Site 2</h2>
 				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
 					mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
 					magna mollis euismod. Donec sed odio dui. </p>
-				<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
 			</div>
 			<!--/span-->
-			<div class="col-6 col-lg-4">
+			<div class="col-12 col-lg-4">
 				<h2>Site 3</h2>
 				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
 					mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
 					magna mollis euismod. Donec sed odio dui. </p>
-				<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
 			</div>
 		</div>
 		<!--/row-->
 
-		<div class="col-12 full-page speak">
-			<div>
-				<h2>Speak</h2>
-				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-					mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-					magna mollis euismod. Donec sed odio dui. </p>
-				<p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+		<div class="row full-page speak">
+			<div class="col-12">
+				<h2>Speaks</h2>
 			</div>
 		</div>
 
 
-		<div class="col-12 full-page contact-form">
-			<div>
+		<div class="row full-page contact-form">
+			<div class="col-12">
 				<h2>Contact Form</h2>
-				<p>
-					Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-					mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-					magna mollis euismod. Donec sed odio dui. 
-				</p>
-			</div>	
+			</div>
 		</div>
-		<!--/span-->
+
 			
 
 	</div>
-	<!--/row-->
-
+</div>
 	<?php get_footer(); ?>
